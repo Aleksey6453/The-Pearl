25a86a955e129f41a974bb81979f604c9578153f
Aleksey6453 <login6453@gmail.com>
2023-04-13 08:43:46 +0300

    start of work for categories.map

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,16 +4,21 @@ const Categories = () => {
 
   const [activeIndex, setActiveIndex] = React.useState(0)
 
+  const categories = ['All', 'Dark', 'Light', 'Colorful']
+
   const onClickCategory = (index) => {
     setActiveIndex(index)
   }
   return (
     <div>
      <ul className='categories'>
-      <li onClick= {()=>onClickCategory(0)} className={activeIndex === 0 ? 'active' : ''}>All</li>
-      <li onClick= {()=>onClickCategory(1)} className={activeIndex === 1 ? 'active' : ''}>Dark</li>
-      <li onClick= {()=>onClickCategory(2)} className={activeIndex === 2 ? 'active' : ''}>Light</li>
-      <li onClick= {()=>onClickCategory(3)} className={activeIndex === 3 ? 'active' : ''}>Colorful</li>
+      {
+        categories.map((el, i)=> (
+          <li onClick= {()=>onClickCategory(0)} className={activeIndex === 0 ? 'active' : ''}>{el + i}</li>
+        )  
+        )
+      }
+      
      </ul>
     </div>
   )
